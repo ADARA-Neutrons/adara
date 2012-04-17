4ddbd1f49cf02ffca9e011aca5a433284cbcc4ff
David Dillow <dillowda@ornl.gov>
2012-04-17 13:12:12 -0400

    Split packets from the parser for maintenance

--- ADARAParser.cc
+++ ADARAParser.cc
@@ -19,28 +19,6 @@ Exception::Exception(int err) : m_error(err)
 
 /* ------------------------------------------------------------------------ */
 
-Packet::Packet(const uint8_t *data, uint32_t len) :
-	PacketHeader(data), m_data(data), m_len(len), m_allocated(false)
-{
-}
-
-Packet::Packet(const Packet &pkt) :
-	PacketHeader(pkt.packet()), m_allocated(true)
-{
-	m_data = new uint8_t[pkt.packet_length()];
-	m_len = pkt.packet_length();
-	memcpy(const_cast<uint8_t *> (m_data), pkt.packet(), m_len);
-}
-
-Packet::~Packet()
-{
-	if (m_allocated)
-		delete [] m_data;
-}
-
-
-/* ------------------------------------------------------------------------ */
-
 Parser::Parser(unsigned int buffer_size, unsigned int max_pkt_size) :
 		m_size(buffer_size), m_max_size(max_pkt_size), m_len(0),
 		m_oversize_len(0)
@@ -244,22 +222,3 @@ EXPAND_HANDLER(DeviceDescriptorPkt)
 EXPAND_HANDLER(VariableU32Pkt)
 EXPAND_HANDLER(VariableDoublePkt)
 EXPAND_HANDLER(VariableStringPkt)
-
-#define EXPAND_CONSTRUCTOR(type) \
-type::type(const uint8_t *data, uint32_t len) : Packet(data, len) { }
-EXPAND_CONSTRUCTOR(RawDataPkt)
-EXPAND_CONSTRUCTOR(RTDLPkt)
-EXPAND_CONSTRUCTOR(BankedEventPkt)
-EXPAND_CONSTRUCTOR(BeamMonitorPkt)
-EXPAND_CONSTRUCTOR(PixelMappingPkt)
-EXPAND_CONSTRUCTOR(RunStatusPkt)
-EXPAND_CONSTRUCTOR(RunInfoPkt)
-EXPAND_CONSTRUCTOR(TransCompletePkt)
-EXPAND_CONSTRUCTOR(ClientHelloPkt)
-EXPAND_CONSTRUCTOR(StatsResetPkt)
-EXPAND_CONSTRUCTOR(SyncPkt)
-EXPAND_CONSTRUCTOR(HeartbeatPkt)
-EXPAND_CONSTRUCTOR(DeviceDescriptorPkt)
-EXPAND_CONSTRUCTOR(VariableU32Pkt)
-EXPAND_CONSTRUCTOR(VariableDoublePkt)
-EXPAND_CONSTRUCTOR(VariableStringPkt)
