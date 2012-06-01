fef455348a28be3883af0ea058f5b782af9d662a
David Dillow <dillowda@ornl.gov>
2012-06-01 02:20:17 -0400

    Actually create map entries
    Using reserve causes later segfaults when assigning handlers, as
    we're not actually adding the elements.

--- SignalEvents.cc
+++ SignalEvents.cc
@@ -18,7 +18,7 @@ void SignalEvents::check_init(void)
 	if (m_fd != -1)
 		return;
 
-	m_sig_map.reserve(SIGRTMAX);
+	m_sig_map.resize(SIGRTMAX);
 
 	sigemptyset(&m_sig_set);
 	m_fd = signalfd(-1, &m_sig_set, SFD_NONBLOCK | SFD_CLOEXEC);
