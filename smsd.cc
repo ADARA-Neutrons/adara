a0a4b1359adbb793f2adf22e97d654c991eca67d
David Dillow <dillowda@ornl.gov>
2012-06-29 17:44:57 -0400

    Initial command line parsing support
    Use Boost::program_options to allow the user to override the logging
    configuration file.

--- smsd.cc
+++ smsd.cc
@@ -8,10 +8,44 @@
 #include "Logging.h"
 
 #include <log4cxx/propertyconfigurator.h>
+#include <boost/program_options.hpp>
+
+namespace po = boost::program_options;
+
+static std::string log_conf("/adara/conf/logging.conf");
+
+static void parse_options(int argc, char **argv)
+{
+	po::options_description desc("Allowed options");
+	desc.add_options()
+		("help,h", "Show usage information")
+		("logconf,l", po::value<std::string>(),
+				"Path to log4cxx property file");
+
+	po::variables_map vm;
+	try {
+		po::store(po::parse_command_line(argc, argv, desc), vm);
+		po::notify(vm);
+	} catch (po::unknown_option e) {
+		std::cerr << argv[0] << ": " << e.what() << std::endl
+			<< std::endl << desc << std::endl;
+		exit(2);
+	}
+
+	if (vm.count("help")) {
+		std::cerr << desc << std::endl;
+		exit(2);
+	}
+
+	if (vm.count("logconf"))
+		log_conf = vm["logconf"].as<std::string>();
+}
 
 int main(int argc, char **argv)
 {
-	PropertyConfigurator::configure("/adara/conf/logging.conf");
+	parse_options(argc, argv);
+
+	PropertyConfigurator::configure(log_conf);
 
 	StorageManager::init("/adara/data");
 	LiveServer liveServer("31415");
