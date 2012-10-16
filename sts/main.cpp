0b59cb2078b13908194df128ea2abdafa94c2612
Dale Stansberry <stansberrydv@ornl.gov>
2012-10-16 12:09:59 -0400

    Updated version and release history.

--- sts/main.cpp
+++ sts/main.cpp
@@ -12,7 +12,7 @@
 
 using namespace std;
 
-#define STS_VERSION "0.1.1"
+#define STS_VERSION "0.1.2"
 
 void
 moveFile( const string &a_source, const string &a_dest_path, const string &a_dest_filename )
