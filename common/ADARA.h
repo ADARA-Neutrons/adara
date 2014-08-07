9682efb611b2169766c500ee0d3e859ffe0b6aee
Jeeem Kohl <kohlja@ornl.gov>
2014-08-07 15:56:41 -0400

    Minor Version Bump for All the Summer Fun. 	- now up to ADARA Common Version 1.1.2.

--- common/ADARA.h
+++ common/ADARA.h
@@ -6,7 +6,7 @@
 
 namespace ADARA {
 
-const std::string VERSION = "1.1.1";
+const std::string VERSION = "1.1.2";
 
 #define ADARA_PKT_TYPE(type, ver)  ((((uint32_t) type) << 8) | (ver))
 namespace PacketType {
