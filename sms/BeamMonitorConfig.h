a1efdb80aac53ebdbb266e69699b50e3b71b729b
Jeeem Kohl <kohlja@ornl.gov>
2015-03-12 15:15:35 -0400

    Add EPICS Live Control PVs to Beam Monitor Config 	- BeamMonitorConfig: m_pvNumBeamMonitors (smsUint32PV) Num of Beam Mons 	- BeamMonitorInfo: indexed set of PVs for each instance 		-> new custom EPICS PV classes for updating each specific field 		of a given BeamMonitorInfo instance, and resetting the Timestamp 		on the Overall Prologue BeamMonitorConfigPkt packet contents 		(using the new BeamMonitorConfig::resetPacketTime() method) 			=> BeamMonIdPV, BeamMonFormatPV (with getFormatEnums()), 			BeamMonOffsetPV, BeamMonMaxPV, BeamMonBinPV, BeamMonDistancePV 		-> pass in new goodies to BeamLineInfo() constructor: 			=> reference to the BeamMonitorConfig parent class (m_config) 			=> new "format" string for instance (m_format) 		-> create, add to SMSControl (addPV()) & init a slew of Indexed PVs: 			=> BeamMonIdPV m_pvId: BLNA:SMS:BeamMonitor:i+1:Id 			=> BeamMonFormatPV m_pvFormat: BLNA:SMS:BeamMonitor:i+1:Format 			=> BeamMonOffsetPV m_pvOffset: 				BLNA:SMS:BeamMonitor:i+1:TofOffset 			=> BeamMonMaxPV m_pvMax: BLNA:SMS:BeamMonitor:i+1:MaxTof 			=> BeamMonBinPV m_pvBin: BLNA:SMS:BeamMonitor:i+1:TofBin 			=> BeamMonDistancePV m_pvDistance: 				BLNA:SMS:BeamMonitor:i+1:Distance 		-> some new accessor methods for Getting BeamMonitorInfo fields: 			=> std::string getFormat() 			=> uint32_t getTofOffset() 			=> uint32_t getTofMax() 			=> uint32_t getTofBin() 			=> double getDistance() 		-> some new accessor methods for Setting BeamMonitorInfo fields: 			=> void setId(uint32_t id) 			=> void setFormat(std::string format) 			=> void setTofOffset(uint32_t tofOffset) 			=> void setTofMax(uint32_t tofMax) 			=> void setTofBin(uint32_t tofBin) 			=> void setDistance(double distance) Also, Re-Deranged the way Beam Monitor Config File options are parsed... 	- for the LiveConf PV changes to mean anything (especially a change 	in "format" from "event" to "histo"... ;-D), we need to keep a 	persistent set of BeamMonitorInfo instances around, whether or not 	the currently agreed upon format is "event" or "histo"... ;-) 		-> unless _No_ Beam Monitor specification found in SMSD Config File, 		_Always_ create the set of BeamMonitorInfo instances 			=> new "format" counters: uint32_t m_numEvent, m_numHisto 			to track _How Many_ Beam Monitors are "voting" for which format 			(if _Any_ Beam Monitor votes "event" it wins, you can't 			histogram a given monitor without the proper parameters...) 		-> decide on-the-fly whether or not to add the Beam Monitor Config 		packet to the Prologue... 			=> TODO: we need to *Only* _Start_ Sending Beam Monitor Config 			on a New Run Boundary!  The STS will capture and use exactly 			_1_ (the first) BeamMonitorConfigPkt, so if we don't send one 			initially we really need to _Never_ send one to that STS... 			(You can't/shouldn't change between Event and Histo midstream.) 			=> Oh Yeah, I forgot to update the new m_numEvent/m_numHisto 			counters on a BeamMonFormatPV::changed() call, so the counts 			actually stay up-to-date... Duh... ;-) It Works...! ;-D 	- as yet incomplete in terms of overall functionality, 	but coming along nicely... ;-D

--- sms/BeamMonitorConfig.h
+++ sms/BeamMonitorConfig.h
@@ -5,23 +5,35 @@
 #include <boost/noncopyable.hpp>
 #include <boost/signals2.hpp>
 
+#include <boost/smart_ptr.hpp>
+
+#include "SMSControl.h"
+#include "SMSControlPV.h"
+
 class BeamMonitorInfo;
+class smsUint32PV;
 
 class BeamMonitorConfig : boost::noncopyable {
 public:
 	BeamMonitorConfig(const boost::property_tree::ptree & conf);
 	~BeamMonitorConfig();
 
+	void resetPacketTime(void);
+
 private:
 	std::vector<BeamMonitorInfo *> bmonInfos;
 
 	uint32_t m_numBeamMonitors;
+	uint32_t m_numEvent;
+	uint32_t m_numHisto;
 
 	uint32_t m_sectionSize;
 	uint32_t m_payloadSize;
 	uint32_t m_packetSize;
 
 	uint8_t *m_packet;
+
+	boost::shared_ptr<smsUint32PV> m_pvNumBeamMonitors;
 
 	boost::signals2::connection m_connection;
 
