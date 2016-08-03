afd481bc528eb06e06495fdf895e5ee483f55ae3
Jeeem Kohl <kohlja@ornl.gov>
2016-08-03 16:52:58 -0400

    Got an OpenLDAP Example Code Working... 	- can use this to programmatically query the LDAP server 	to resolve XCAMS/UCAMS User IDs to Full Names for the NeXus files... 		-> IPTS web service now only returns User ID... 			=> will apply this code example to resolve the 			"/entry/sample/name" field from "facility_user_id" 			=> also, will remove the useless "role" attribute 	- It Works...! ;-D 		-> the OpenLDAP documentation online is _Pathetic_ in 		many ways, lacks full examples, and half of the original 		OpenLDAP calls are now deprecated, but with some luck, 		header file perusing and good ol' trial and error, 		it finally worked...! ;-D 			=> e.g.: 			Connected to LDAP Server data.sns.gov at port 389 			Set LDAP Protocol to Version 3. 			Searched LDAP Server for filter[0]=uid=FENG_BNL 			Got 1 Entries from LDAP 			LDAP Message. 			LDAP Attribute attr=cn 			cn:Wang, Feng 			Searched LDAP Server for filter[1]=uid=WANGDAWEI 			Got 1 Entries from LDAP 			LDAP Message. 			LDAP Attribute attr=cn 			cn:Wang, Dawei 			Searched LDAP Server for filter[2]=uid=y8y 			Got 1 Entries from LDAP 			LDAP Message. 			LDAP Attribute attr=cn 			cn:Kohl, James Arthur 			Ended Session with LDAP Server.

---
+++ sts/UserIdLdap.c
@@ -0,0 +1,104 @@
+//
+// User ID LDAP Lookup
+//
+// gcc -o UserIdLdap UserIdLdap.c -lldap -llber
+//
+
+#include <stdio.h>
+#include <ldap.h>
+
+int main()
+{
+	LDAP *ld;
+
+	LDAPMessage *msg;
+
+	char *ldap_host = "data.sns.gov";
+	char ldap_uri[255];
+
+	char *base = "ou=Users,dc=sns,dc=ornl,dc=gov";
+	char *filter[3];
+	char *attrs[2];
+
+	int desired_version = LDAP_VERSION3;
+	int auth_method = LDAP_AUTH_SIMPLE;
+
+	int result;
+
+	// Connect to the LDAP Server
+	sprintf( ldap_uri, "ldap://%s:%d", ldap_host, LDAP_PORT );
+	if ( ldap_initialize( &ld, ldap_uri ) != LDAP_SUCCESS ) {
+		perror("ldap_initialize failed");
+		return( -1 );
+	}
+	printf( "Connected to LDAP Server %s at port %d\n",
+		ldap_host, LDAP_PORT );
+
+	// Set LDAP Protocol to Version 3
+	if ( ldap_set_option(ld, LDAP_OPT_PROTOCOL_VERSION, &desired_version)
+			!= LDAP_OPT_SUCCESS ) {
+	    ldap_perror(ld, "ldap_set_option failed!");
+	    return( -1 );
+	}
+	printf( "Set LDAP Protocol to Version 3.\n" );
+
+	// Set Up LDAP Search Filters...
+	filter[0] = "uid=FENG_BNL";
+	filter[1] = "uid=WANGDAWEI";
+	filter[2] = "uid=y8y";
+
+	// Choose Desired Attribute(s)...
+	attrs[0] = "cn";
+	attrs[1] = NULL;
+
+	// Search for Each User in Turn...
+	int j;
+	for ( j=0; j < 3 ; j++ )
+	{
+		// Search LDAP Server for User ID
+		if ( ldap_search_ext_s(ld, base, LDAP_SCOPE_SUBTREE, filter[j],
+				(char **)attrs, 0, NULL, NULL, NULL, LDAP_NO_LIMIT, &msg)
+					!= LDAP_SUCCESS)
+		{
+			ldap_perror( ld, "ldap_search_s" );
+		}
+		printf( "Searched LDAP Server for filter[%d]=%s\n", j, filter[j] );
+
+		// Display LDAP Search Results
+		int num_entries_returned = ldap_count_entries(ld, msg);
+		printf( "Got %d Entries from LDAP\n", num_entries_returned );
+
+		LDAPMessage *entry;
+		for ( entry=ldap_first_entry(ld, msg); entry != NULL;
+	    		entry = ldap_next_entry(ld, entry) )
+		{
+			printf( "LDAP Message.\n" );
+			BerElement *ber;
+			char *attr;
+			for( attr=ldap_first_attribute(ld, entry, &ber); attr != NULL;
+	    			attr = ldap_next_attribute(ld, entry, ber) ) {
+				printf( "LDAP Attribute attr=%s\n", attr );
+
+				struct berval **vals;
+				if ( (vals=ldap_get_values_len(ld, entry, attr)) != NULL )
+				{
+					int i;
+					for ( i=0; vals[i] != NULL; i++ ) {
+						printf("%s:%s\n", attr, vals[i]->bv_val);
+					}
+				}
+			}
+		}
+	}
+
+	// End Session with LDAP Server
+	result = ldap_unbind_s(ld);
+	if ( result != 0 ) {
+		fprintf( stderr, "ldap_unbind_s: %s\n", ldap_err2string(result) );
+		return( -1 );
+	}
+	printf( "Ended Session with LDAP Server.\n" );
+
+	return( 0 );
+}
+
