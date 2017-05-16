4379adcb547627f7c9788be63e1c5a653a3e1bf5
Jeeem Kohl <kohlja@ornl.gov>
2017-05-16 16:20:34 -0400

    Add New Public STS::StreamParser Accessor Method 	- "bool getNoSampleInfo()" 		-> returns public access to "STS::RunInfo::no_sample_info" 			=> for some tricks and tweaks in NxGen... ;-D 	- minor text formatting... :-)

--- sts/StreamParser.h
+++ sts/StreamParser.h
@@ -41,11 +41,19 @@ public:
     void    printStats( std::ostream &a_os ) const;
     void    getXmlNodeValue( xmlNode *a_node, std::string & a_value ) const;
 
-    std::string getFacilityName() const { return m_run_info.facility_name; }
-    std::string getBeamShortName() const { return m_run_info.instr_shortname; }
-    std::string getProposalID() const { return m_run_info.proposal_id; }
-    uint32_t    getRunNumber() const { return m_run_info.run_number; }
-    bool        infoReady() const { return (m_info_rcvd & INFO_SENT); }
+    std::string getFacilityName() const
+        { return m_run_info.facility_name; }
+    std::string getBeamShortName() const
+        { return m_run_info.instr_shortname; }
+    std::string getProposalID() const
+        { return m_run_info.proposal_id; }
+    uint32_t getRunNumber() const
+        { return m_run_info.run_number; }
+    bool getNoSampleInfo() const
+        { return m_run_info.no_sample_info; }
+
+    bool infoReady() const
+        { return (m_info_rcvd & INFO_SENT); }
 
 private:
     /// Defines internal stream processing states of StreamParser class
