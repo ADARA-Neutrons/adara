1bf3ea54b9ace4d673a47487e4f011aa0b925fd1
Jeeem Kohl <kohlja@ornl.gov>
2018-02-02 15:13:07 -0500

    *** Extended EventFd Class for Arbitrary Data! 	- generalized for new ComBusSMSMon commThread() handling...! ;-D 	- no longer assumes "uint64_t value" data being communicated 	between threads (via file descriptor... :-) 		-> parameterized all the methods to take: 			=> void *buffer, ssize_t buffer_length 		-> methods no longer return "uint64_t", just "void"... 		(since these methods eventually move all the expected data 		or else die with an exception anyway...! ;-D) 		-> uint64_t read(void) 			=> void read(void *buffer, ssize_t buffer_length) 		-> uint64_t block(void) 			=> void block(void *buffer, ssize_t buffer_length) 		-> void signal(uint64_t val = 1) 			=> void signal(void *buffer, ssize_t buffer_length) 			=> also, lose the "val=1" default, never used anyway 		-> uint64_t do_read(void) 			=> void do_read(void *buffer, ssize_t buffer_length) 			=> also, since we won't always just be sending thru 			8 bytes here any more, _Make Sure_ we keep trying 			to read until _All_ the expected bytes are read 			and the buffer is full...! (Rookie mistake, lol...) 		-> added new "do_write()" method, too, like new do_read(), 		which ensures that _All_ requested bytes are sent...! ;-D 			=> void do_write(void *buffer, ssize_t buffer_len) 	- Lots of Debug Logging Added for Testing/Verification...! ;-D 		-> when we're all done here, this should probably be cleaned

--- sms/EventFd.h
+++ sms/EventFd.h
@@ -11,57 +11,59 @@
 #include <stdexcept>
 
 #include "ReadyAdapter.h"
+#include "Logging.h"
 
 class EventFd {
 public:
 	typedef boost::function<void (fdRegType)> callback;
 
 	EventFd() { init(0); }
-	EventFd(callback cb) {
-		init(EFD_NONBLOCK);
-		m_ready.reset(new ReadyAdapter(m_fd, fdrRead, cb));
+
+	EventFd( callback cb ) {
+		init( EFD_NONBLOCK );
+		m_ready.reset(new ReadyAdapter( m_fd, fdrRead, cb ));
 	}
 
 	/* TODO some day, we may need to try to read from a blocking object
 	 * without blocking; at that point we can revisit the guards we
 	 * have in place.
 	 */
-	uint64_t read(void) {
-		if (!m_ready.get()) {
+	void read( void *buffer, ssize_t buffer_length ) {
+		if ( !m_ready.get() ) {
 			throw std::runtime_error("Calling EventFd::read on a "
 						 "blocking object");
 		}
 
-		return do_read();
+		do_read( buffer, buffer_length );
 	}
 
-	uint64_t block(void) {
-		if (m_ready.get()) {
+	void block( void *buffer, ssize_t buffer_length ) {
+		if ( m_ready.get() ) {
 			throw std::runtime_error("Calling EventFd::block on a "
 						 "non-blocking object");
 		}
 
-		return do_read();
+		do_read( buffer, buffer_length );
 	}
 
-	void signal(uint64_t val = 1) {
-		if (write(m_fd, &val, sizeof(val)) != sizeof(val)) {
-			int e = errno;
-			std::string msg("Unable to write to eventfd: ");
-			msg += strerror(e);
-			throw std::runtime_error(msg);
-		}
+	void signal( void *buffer, ssize_t buffer_length ) {
+
+		DEBUG("EventFd signal():"
+			<< " buffer=0x" << std::hex << buffer << std::dec
+			<< " buffer_length=" << buffer_length);
+
+		do_write( buffer, buffer_length );
 	}
 
-	~EventFd() { close(m_fd); }
+	~EventFd() { close( m_fd ); }
 
 private:
 	std::auto_ptr<ReadyAdapter> m_ready;
 	int m_fd;
 
-	void init(int flags) {
-		m_fd = eventfd(0, flags);
-		if (m_fd < 0) {
+	void init( int flags ) {
+		m_fd = eventfd( 0, flags );
+		if ( m_fd < 0 ) {
 			int e = errno;
 			std::string msg("Unable to create eventfd: ");
 			msg += strerror(e);
@@ -69,23 +71,88 @@ private:
 		}
 	}
 
-	uint64_t do_read(void) {
-		uint64_t val;
+	void do_read( void *buffer, ssize_t buffer_length ) {
+
+		char *bufptr = (char *) buffer;
+		ssize_t len = buffer_length;
 		ssize_t rc;
 
-		// NOTE: This is Standard C Library read()... ;-o
-		rc = ::read(m_fd, &val, sizeof(val));
-		if (rc != 8) {
-			if (errno != EAGAIN && errno != EINTR) {
-				int e = errno;
-				std::string msg("Unable to read eventfd: ");
-				msg += strerror(e);
-				throw std::runtime_error(msg);
-			} else
-				val = ~0;
+		DEBUG("EventFd do_read():"
+			<< " buffer=0x" << std::hex << buffer << std::dec
+			<< " buffer_length=" << buffer_length);
+
+		while ( len > 0 )
+		{
+			DEBUG("EventFd do_read(): Reading Value from Fd"
+				<< " bufptr=0x" << std::hex << (void *) bufptr << std::dec
+				<< " remaining len=" << len);
+
+			// NOTE: This is Standard C Library read()... ;-o
+			rc = ::read( m_fd, bufptr, len );
+
+			if ( rc == (ssize_t) -1 ) {
+				DEBUG("EventFd do_read(): Read Returned rc=" << rc);
+				if (errno != EAGAIN && errno != EINTR) {
+					int e = errno;
+					std::string msg("Unable to read eventfd: ");
+					msg += strerror(e);
+					throw std::runtime_error(msg);
+				} else {
+					DEBUG("EventFd do_read(): Continuing...");
+					//val = ~0;
+				}
+			}
+			else {
+				DEBUG("EventFd do_read(): Read " << rc << " Bytes "
+					<< "out of " << len << " Requested.");
+				bufptr += rc;
+				len -= rc;
+			}
 		}
 
-		return val;
+		DEBUG("EventFd do_read(): Done. "
+			<< "Read " << buffer_length << " Bytes.");
+	}
+
+	void do_write( void *buffer, ssize_t buffer_length ) {
+
+		char *bufptr = (char *) buffer;
+		ssize_t len = buffer_length;
+		ssize_t rc;
+
+		DEBUG("EventFd do_write():"
+			<< " buffer=0x" << std::hex << buffer << std::dec
+			<< " buffer_length=" << buffer_length);
+
+		while ( len > 0 )
+		{
+			DEBUG("EventFd do_write(): Writing Value to Fd"
+				<< " bufptr=0x" << std::hex << (void *) bufptr << std::dec
+				<< " remaining len=" << len);
+
+			rc = write( m_fd, bufptr, len );
+
+			if ( rc == (ssize_t) -1 ) {
+				DEBUG("EventFd do_write(): Write Returned rc=" << rc);
+				if (errno != EAGAIN && errno != EINTR) {
+					int e = errno;
+					std::string msg("Unable to write to eventfd: ");
+					msg += strerror(e);
+					throw std::runtime_error(msg);
+				} else {
+					DEBUG("EventFd do_write(): Continuing...");
+				}
+			}
+			else {
+				DEBUG("EventFd do_write(): Wrote " << rc << " Bytes "
+					<< "out of " << len << " Requested.");
+				bufptr += rc;
+				len -= rc;
+			}
+		}
+
+		DEBUG("EventFd do_write(): Done. "
+			<< "Wrote " << buffer_length << " Bytes.");
 	}
 };
 
