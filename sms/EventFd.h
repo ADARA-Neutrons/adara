be9e87271e9bc7b124491182bcbd392eeba2ee76
Jeeem Kohl <kohlja@ornl.gov>
2018-02-06 14:32:19 -0500

    Reduced/Streamlined Logging in EventFd Class... 	- comment out some Debug Logging 	- apply Std::StringStream to collect data reading/writing statistics 	- only dump logging on successful read/write or errors 		-> i.e. don't log non-blocking reads that don't get any data

--- sms/EventFd.h
+++ sms/EventFd.h
@@ -4,6 +4,7 @@
 #include <boost/function.hpp>
 #include <fdManager.h>
 #include <sys/eventfd.h>
+#include <sstream>
 #include <stdint.h>
 #include <errno.h>
 
@@ -86,50 +87,54 @@ private:
 		ssize_t len = sizeof(val);
 		ssize_t rc;
 
+		std::stringstream ss;
+
 		uint32_t cnt = 0;
 
 		while ( len > 0 && cnt++ < 3 )
 		{
-			DEBUG("EventFd do_read(): Reading Value from Fd"
-				<< " bufptr=0x" << std::hex << (void *) bufptr << std::dec
-				<< " remaining len=" << len);
+			//DEBUG("EventFd do_read(): Reading Value from Fd"
+				//<< " bufptr=0x" << std::hex << (void *) bufptr << std::dec
+				//<< " remaining len=" << len);
 
 			// NOTE: This is Standard C Library read()... ;-o
 			rc = ::read( m_fd, bufptr, len );
 
 			if ( rc == (ssize_t) -1 ) {
-				DEBUG("EventFd do_read(): Read Returned rc=" << rc);
 				if (errno != EAGAIN && errno != EINTR) {
 					int e = errno;
 					std::string msg("Unable to read eventfd: ");
 					msg += strerror(e);
 					throw std::runtime_error(msg);
-				} else {
-					DEBUG("EventFd do_read(): Continuing..."
-						<< " (cnt=" << cnt << ")");
+				} else if ( !m_nonBlocking ) {
+					DEBUG("EventFd do_read(): Read Returned rc=" << rc
+						<< " Continuing..." << " (cnt=" << cnt << ")");
 				}
 			}
 			else {
-				DEBUG("EventFd do_read(): Read " << rc << " Bytes "
-					<< "out of " << len << " Requested.");
+				ss << "Read " << rc << " Bytes "
+					<< "out of " << len << " Requested. ";
 				bufptr += rc;
 				len -= rc;
 			}
 		}
 
 		if ( len == 0 ) {
-			DEBUG("EventFd do_read(): Done. "
+			DEBUG("EventFd do_read(): " << ss.str() << "Done. "
 				<< "Read " << sizeof(val) << " Bytes. "
 				<< "Value = " << val << "/0x"
 					<< std::hex << val << std::dec);
 			return( true );
 		}
 		else {
-			ERROR("EventFd do_read(): Value Not Read! "
-				<< "Only Read " << ( sizeof(val) - len ) << " Bytes "
-				<< "out of " << sizeof(val) << " Requested. "
-				<< "Value = " << val << "/0x"
-					<< std::hex << val << std::dec);
+			if ( !m_nonBlocking || len != sizeof(val) ) {
+				ERROR("EventFd do_read(): " << ss.str()
+					<< "Value Not Fully Read! "
+					<< "Only Read " << ( sizeof(val) - len ) << " Bytes "
+					<< "out of " << sizeof(val) << " Requested. "
+					<< "Value = " << val << "/0x"
+						<< std::hex << val << std::dec);
+			}
 			// val = ~0;
 			return( false );
 		}
@@ -141,45 +146,47 @@ private:
 		ssize_t len = sizeof(val);
 		ssize_t rc;
 
+		std::stringstream ss;
+
 		uint32_t cnt = 0;
 
 		while ( len > 0 && cnt++ < 3 )
 		{
-			DEBUG("EventFd do_write(): Writing Value to Fd"
-				<< " bufptr=0x" << std::hex << (void *) bufptr << std::dec
-				<< " remaining len=" << len);
+			//DEBUG("EventFd do_write(): Writing Value to Fd"
+				//<< " bufptr=0x" << std::hex << (void *) bufptr << std::dec
+				//<< " remaining len=" << len);
 
 			rc = write( m_fd, bufptr, len );
 
 			if ( rc == (ssize_t) -1 ) {
-				DEBUG("EventFd do_write(): Write Returned rc=" << rc);
 				if (errno != EAGAIN && errno != EINTR) {
 					int e = errno;
 					std::string msg("Unable to write to eventfd: ");
 					msg += strerror(e);
 					throw std::runtime_error(msg);
 				} else {
-					DEBUG("EventFd do_write(): Continuing..."
-						<< " (cnt=" << cnt << ")");
+					DEBUG("EventFd do_write(): Write Returned rc=" << rc
+						<< " Continuing..." << " (cnt=" << cnt << ")");
 				}
 			}
 			else {
-				DEBUG("EventFd do_write(): Wrote " << rc << " Bytes "
-					<< "out of " << len << " Requested.");
+				ss << "Wrote " << rc << " Bytes "
+					<< "out of " << len << " Requested. ";
 				bufptr += rc;
 				len -= rc;
 			}
 		}
 
 		if ( len == 0 ) {
-			DEBUG("EventFd do_write(): Done. "
+			DEBUG("EventFd do_write(): " << ss.str() << "Done. "
 				<< "Wrote " << sizeof(val) << " Bytes. "
 				<< "Value = " << val << "/0x"
 					<< std::hex << val << std::dec);
 			return( true );
 		}
 		else {
-			ERROR("EventFd do_write(): Value Not Written! "
+			ERROR("EventFd do_write(): " << ss.str()
+				<< "Value Not Written! "
 				<< "Only Wrote " << ( sizeof(val) - len ) << " Bytes "
 				<< "out of " << sizeof(val) << " Requested. "
 				<< "Value = " << val << "/0x"
