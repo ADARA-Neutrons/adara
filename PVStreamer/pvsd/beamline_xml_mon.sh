06817884c26b71cf362c1b4d05595e609dcfeca4
Jeeem Kohl <kohlja@ornl.gov>
2020-02-03 15:41:03 -0500

    Filled In Main Functionality for Beamline XML Mon 	- check for local diffs, as well as committed but not yet pushed... 	- added "--quiet" operation for Cron usage... ;-D

--- PVStreamer/pvsd/beamline_xml_mon.sh
+++ PVStreamer/pvsd/beamline_xml_mon.sh
@@ -9,7 +9,104 @@
 # Source Local EPICS Beamline Environment (e.g. $BEAMLINE):
 . /home/controls/share/master/scripts/beamline_profile.sh
 
-cd "/home/controls/$BEAMLINE"
+# Handle "Quiet" Operation Option for Cron Usage...
+# (Only output on errors...)
+QUIET=0
+if [ "$1" == "--quiet" ]; then
+	QUIET=1
+fi
 
-git diff beamline.xml
+# Go to proper Git Config Repo Checkout, for easy checking systemwide.
+BEAMLINE_GIT_DIR="/home/controls/$BEAMLINE"
+
+BEAMLINE_XML="beamline.xml"
+
+cd "${BEAMLINE_GIT_DIR}"
+
+# Check for Local Uncommitted Changes
+CHECK_BEAMLINE_XML_DIFF()
+{
+	local _err=0
+
+	git diff ${BEAMLINE_XML}
+	local _status=$?
+	if [[ "${_status}" != 0 ]]; then
+		_err="${_status}"
+	fi
+
+	return "${_err}"
+}
+
+# Check for Committed But Not Yet Pushed Changes...!
+CHECK_BEAMLINE_XML_DIFF_ORIGIN()
+{
+	local _err=0
+
+	git diff origin/master ${BEAMLINE_XML}
+	local _status=$?
+	if [[ "${_status}" != 0 ]]; then
+		_err="${_status}"
+	fi
+
+	return "${_err}"
+}
+
+CHECK_BEAMLINE_XML()
+{
+	local _err=0
+
+	# Check for Local Uncommitted Changes
+	# (DON'T Save Results in Local Variable, Eats Up Return Status!)
+	diffCk=`CHECK_BEAMLINE_XML_DIFF 2>&1`
+	local _status=$?
+	if [[ "${_status}" != 0 ]]; then
+		_err="${_status}"
+	fi
+	if [[ -n "${diffCk}" ]]; then
+		echo -e "Uncommitted Local Changes:\n"
+		echo "${diffCk}"
+	fi
+
+	# Also Check for Committed But Not Yet Pushed Changes...!
+	# (DON'T Save Results in Local Variable, Eats Up Return Status!)
+	diffCkOrigin=`CHECK_BEAMLINE_XML_DIFF_ORIGIN 2>&1`
+	local _status=$?
+	if [[ "${_status}" != 0 ]]; then
+		_err="${_status}"
+	fi
+	if [[ "${diffCkOrigin}" != "${diffCk}" ]]; then
+		echo -e "Committed Local Changes Not Yet Pushed to Origin:\n"
+		echo "${diffCkOrigin}"
+	fi
+
+	return "${_err}"
+}
+
+doCkBeamlineXML=`CHECK_BEAMLINE_XML 2>&1`
+_status=$?
+
+if [[ "${_status}" != 0 || -n "${doCkBeamlineXML}" || $QUIET == 0 ]]; then
+	echo -e "\nBeamline XML Configuration Change Check:\n"
+fi
+
+if [[ "${_status}" != 0 ]]; then
+	echo -n "*** Error Checking "
+	echo -e "Beamline XML for Config Changes!\n"
+	/bin/ls -l "${BEAMLINE_GIT_DIR}/${BEAMLINE_XML}" \
+		| sed "s@ ${BEAMLINE_GIT_DIR}@\n   ${BEAMLINE_GIT_DIR}@"
+	echo
+elif [[ -n "${doCkBeamlineXML}" ]]; then
+	echo -n "*** Uncommitted or Unpushed "
+	echo -e "Beamline XML Config Changes Found!\n"
+	/bin/ls -l "${BEAMLINE_GIT_DIR}/${BEAMLINE_XML}" \
+		| sed "s@ ${BEAMLINE_GIT_DIR}@\n   ${BEAMLINE_GIT_DIR}@"
+	echo
+fi
+
+if [[ "${_status}" != 0 || -n "${doCkBeamlineXML}" || $QUIET == 0 ]]; then
+	if [[ -n "${doCkBeamlineXML}" ]]; then
+		echo -e "$doCkBeamlineXML\n"
+	fi
+	echo -e "Status = ${_status}\n"
+fi
 
