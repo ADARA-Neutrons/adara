e47f3166010be7b440d65ff3bc9bb8e6a014f769
Jeeem Kohl <kohlja@ornl.gov>
2020-11-12 14:15:49 -0500

    Added "Verbose" Parameter to ReadyAdapter() Ctor 	- to stem the tide of excessive SMS logging... ;-D 		-> passed in from "uint32_t m_verbose" in SMSControl... 	- bumped ADARA Common Library Version: 		-> VERSION = "1.9.7" -> "1.9.8"

--- common/ReadyAdapter.h
+++ common/ReadyAdapter.h
@@ -10,21 +10,31 @@ class ReadyAdapter : public fdReg {
 public:
 	typedef boost::function<void (fdRegType)> callback;
 
-	ReadyAdapter(int fd, fdRegType reg, callback cb) :
-			fdReg(fd, reg), m_cb(cb), m_fd(fd), m_reg(reg)
+	ReadyAdapter(int fd, fdRegType reg, callback cb, uint32_t verbose) :
+			fdReg(fd, reg), m_cb(cb), m_fd(fd), m_reg(reg),
+			m_verbose(verbose)
 	{
-		DEBUG("ReadyAdapter Created m_fd=" << m_fd << " reg=" << m_reg);
+		if ( m_verbose > 0 )
+		{
+			DEBUG("ReadyAdapter Created m_fd=" << m_fd
+				<< " reg=" << m_reg);
+		}
 	}
 
 	~ReadyAdapter()
 	{
-		DEBUG("ReadyAdapter Destroyed m_fd=" << m_fd << " reg=" << m_reg);
+		if ( m_verbose > 0 )
+		{
+			DEBUG("ReadyAdapter Destroyed m_fd=" << m_fd
+				<< " reg=" << m_reg);
+		}
 	}
 
 private:
 	callback m_cb;
-int m_fd;
-fdRegType m_reg;
+	int m_fd;
+	fdRegType m_reg;
+	uint32_t m_verbose;
 
 	void callBack(void) { m_cb(getType()); }
 };
